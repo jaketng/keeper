65125fef559d19606a87e6dfaea6c6d51eb907b5
Jake Bowden <jakebowden@Jakes-MacBook-Pro-2.local>
2023-12-12 14:31:06 -0500

    Backend Working

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import { notes as notesData } from "../notes";
+import axios from "axios";
 
 function App() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [notes, setNotes] = useState(notesData);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   function handleTitleChange(event) {
     setTitle(event.target.value);
@@ -17,20 +21,39 @@ function App() {
     setContent(event.target.value);
   }
 
-  function addNote(event) {
-    event.preventDefault();
-    const newNote = {
-      key: notes.length + 1,
-      title: title,
-      content: content
-    };
-    setNotes([...notes, newNote]);
-    setTitle("");
-    setContent("");
+  async function fetchData() {
+    try {
+      const response = await axios.get("http://localhost:5001/api/postData");
+      setNotes(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   }
 
-  function deleteNote(key) {
-    setNotes((prevNotes) => prevNotes.filter((note) => note.key !== key));
+  async function addNote(event) {
+    event.preventDefault();
+    const newNote = {
+      title: title,
+      content: content,
+    };
+
+    try {
+      await axios.post("http://localhost:5001/api/postData", newNote);
+      fetchData();
+      setTitle("");
+      setContent("");
+    } catch (error) {
+      console.error("Error adding note:", error);
+    }
+  }
+
+  async function deleteNote(id) {
+    try {
+      await axios.delete(`http://localhost:5001/api/postData/${id}`);
+      fetchData();
+    } catch (error) {
+      console.error("Error deleting note:", error);
+    }
   }
 
   return (
@@ -52,10 +75,10 @@ function App() {
       </form>
       {notes.map((note) => (
         <Note
-          key={note.key}
+          key={note._id}
           title={note.title}
           content={note.content}
-          onDelete={() => deleteNote(note.key)}
+          onDelete={() => deleteNote(note._id)}
         />
       ))}
       <Footer />
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
+const bodyParser = require('body-parser');
+
+app.use(cors()); 
+app.use(bodyParser.json());
 
 uri = 'mongodb+srv://jakepb02:Andover101@testcluster.d03ektu.mongodb.net/?retryWrites=true&w=majority';
 
@@ -18,8 +21,47 @@ async function connect() {
 
 connect();
 
-app.get("/api", (req, res) => {
-    res.json({ "users": ["user1", "user2"] })
-})
+const noteSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+  });
+  
+const Note = mongoose.model("Note", noteSchema);
 
-app.listen(5001, () => { console.log("Server started on port 5001") })
+app.post("/api/postData", async (req, res) => {
+    console.log("req received");
+    const data = req.body;
+  
+    const newNote = new Note(data);
+    try {
+      const result = await newNote.save();
+      res.json(result);
+    } catch (error) {
+      res.send(error);
+    }
+  });
+  
+app.get("/api/postData", async (req, res) => {
+try {
+    const notes = await Note.find();
+    res.json(notes);
+} catch (error) {
+    res.send(error);
+}
+});
+
+app.delete("/api/postData/:id", async (req, res) => {
+const id = req.params.id;
+try {
+    const result = await Note.findByIdAndDelete(id);
+    res.json(result);
+} catch (error) {
+    res.send(error);
+}
+});
+
+const PORT = 5001; 
+
+app.listen(PORT, () => {
+console.log(`Server started on port ${PORT}`);
+});
